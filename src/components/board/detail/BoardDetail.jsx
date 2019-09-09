b7e9a787b490162317b014d5035d37d98e1579f7
hyeoni90 <gusk743@gmail.com>
2019-09-09 21:34:53 +0900

    board Detail component

---
+++ src/components/board/detail/BoardDetail.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BoardDetail extends Component {
+  render() {
+    return (
+        <div>
+          <h3>BoardDetails</h3>
+        </div>
+    )
+  }
+}
+
+export default BoardDetail;
